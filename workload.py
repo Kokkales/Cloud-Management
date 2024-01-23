ad08845ae19f9d8c7b3f90025b5e9acb72cf00f9
Konstantinos Kokkalis <kokkaleskonstantinos@gmail.com>
2024-01-23 17:05:57 +0200

    UPDATE: requirements.txt
    necessary packages to install
    pip install -r requirements.txt

---
+++ workload.py
@@ -0,0 +1,41 @@
+class WorkloadCreator():
+    def __init__(self,load):
+        self.load=load
+
+    # Stable Load
+    def create_stable_load():
+        # Create a number of requests
+        # seperate the requests into same size batches
+        # set sleep time per batch
+        return
+
+    # Normal Load
+    def create_normal_load():
+        # Create a number of requests
+        # seperate the requests into random size batches
+        # set sleep time per batch
+        return
+
+    # Peak Load
+    def create_peak_load():
+        # Create a number of requests
+        # seperate the requests into random size batches
+        # in one random batch triple the requests
+        # set sleep time per batch
+        return
+
+    # Spike Load
+    def create_spike_load():
+        # Create a number of requests
+        # seperate the requests into random size batches
+        # in one random batch triple the requests
+        # set sleep time per batch
+        return
+
+    # # Data Volume Load
+    # def create_data_volume_load():
+    #     return
+
+    # Stress Load - EXTRA
+    def create_stress_load():
+        return
